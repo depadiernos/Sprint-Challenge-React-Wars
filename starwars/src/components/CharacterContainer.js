5fd0a606aadc5489e03fb26249cf52e085b111da
Dennis Padiernos <depadiernos@gmail.com>
2019-08-30 20:44:44 -0500

    add load spinner

--- starwars/src/components/CharacterContainer.js
+++ starwars/src/components/CharacterContainer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CharacterCard from './CharacterCard'
 import styled from 'styled-components'
+import Loader from 'react-loader-spinner'
 
 const CharacterContainer = styled.div`
   display: flex;
@@ -11,8 +12,13 @@ const CharacterContainer = styled.div`
 
 export default function ({ characters }) {
   return <CharacterContainer>
-    {(characters) && (characters.results) && characters.results.map((character, index) => {
+    {(characters && characters.results) ? characters.results.map((character, index) => {
       return <CharacterCard key={index} character={character} />
-    })}
+    }) : <Loader
+        type="Puff"
+        color="beige"
+        height={300}
+        width={300}
+      />}
   </CharacterContainer>
 }
